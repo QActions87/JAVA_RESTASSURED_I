386ffe7a0a149972e2209a42a5d68ba566bf5952
Tiago Mota <tmota.seg@gmail.com>
2026-09-08 15:49:11 -0300

    Feat: Adicionando comentários ao 1º teste.

--- restassured-testeapi/src/test/java/PrimeiraApiTeste.java
+++ restassured-testeapi/src/test/java/PrimeiraApiTeste.java
@@ -9,18 +9,28 @@ import org.junit.Test;
 
 public class PrimeiraApiTeste {
 
+    // Indica ao JUnit que este método é um teste automatizado a ser executado
     @Test
     public void testeComSucessoApi() {
+        // Armazena a URL base e o endpoint que será testado
         String url = "http://localhost:8080/api/primeiraApi";
-        // Chamando o metodo 'RestAssured' de forma explícita:
+
+        // Dado que: Inicia a preparação da requisição com a chamada explícita da classe RestAssured
         RestAssured.given()
+                        // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request)
                         .log().all()
+                    // Quando: Executa a ação principal do teste (disparo do verbo HTTP)
                     .when()
-                        .get(url)
+                        // Envia uma requisição HTTP do tipo GET para o endereço da variável 'url'
+                         .get(url)
+                     // Então: Inicia o bloco de validações e asserções da resposta (Response)
                     .then()
+                        // Imprime no console todos os detalhes do que foi RECEBIDO do servidor
                         .log().all()
+                         // Metodo de transição BDD que melhora a leitura do código (afirme que...)
                         .assertThat()
-                        .statusCode(200);
+                        // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK)
+                         .statusCode(200);
     }
 }
 
