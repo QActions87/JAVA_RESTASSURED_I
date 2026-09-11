7b7bd19216849b3e12c49f742c7d794d67f49fba
Tiago Mota <tmota.seg@gmail.com>
2026-09-11 17:45:54 -0300

    Feat: 6º Exercício:  2º do endpoit trabalhado (exercicios), dando um get no query param 'validarCpf' com a chave e valor válidos, validando a mensagem de sucesso do response.

--- restassured-testeapi/src/test/java/EndPointExerciciosTests.java
+++ restassured-testeapi/src/test/java/EndPointExerciciosTests.java
@@ -67,4 +67,39 @@ public class EndPointExerciciosTests {
                     // Valida se o corpo (body) da resposta contém a frase/texto especificado:
                     .body(Matchers.containsString(mensagemResponsePathParam));
     }
+
+    // 1º Teste: verifica se a variável passada por Query Param é a mesma retornada no payload do response:
+    @Test
+    public void exercicio05_TesteValidacaoCPF() {
+        // Armazena a URL base e o endpoint que será testado:
+        String url = "http://localhost:8080/exercicios/validarCpf";
+        // Armazena o Valor da chave do query param:
+        String chaveQueryParam = "cpf";
+        // Armazena o Valor da chave do query param:
+        String valorQueryParam = "13948436070";
+        // Armazena a mensagem do response do query param:
+        String mensagemResponseQueryParam = "CPF Valido";
+
+        // Dado que: Inicia a preparação da requisição com a chamada explícita da classe RestAssured:
+        RestAssured.given()
+                // Passando o parâmetro e valor do query param:
+                .queryParam(chaveQueryParam, valorQueryParam)
+                // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request):
+                .log().all()
+                // Quando: Executa a ação principal do teste (disparo do verbo HTTP):
+                .when()
+                // Envia uma requisição HTTP do tipo GET para o endereço da variável 'url':
+                .get(url)
+                // Então: Inicia o bloco de validações e asserções da resposta (Response):
+                .then()
+                // Imprime no console todos os detalhes do que foi RECEBIDO do servidor:
+                .log().all()
+                // Metodo de transição BDD que melhora a leitura do código (afirme que...).
+                // Indica que a partir daqui virão às asserções:
+                .assertThat()
+                // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK):
+                .statusCode(200)
+                // Valida se o corpo (body) da resposta contém a frase/texto especificado:
+                .body(Matchers.containsString(mensagemResponseQueryParam));
+    }
 }
