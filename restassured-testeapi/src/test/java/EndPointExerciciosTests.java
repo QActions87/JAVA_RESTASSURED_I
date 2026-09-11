f2ba0f3bf1dd9eac063a7d7df92f5cad15299376
Tiago Mota <tmota.seg@gmail.com>
2026-09-11 17:23:20 -0300

    Feat: 5º Exercício:  2º do endpoit trabalhado (exercicios), dando um get no path param 'calculaSalario', passando o valor de 2600 e verificando se o valor bruto foi retornado no payload.

--- restassured-testeapi/src/test/java/EndPointExerciciosTests.java
+++ restassured-testeapi/src/test/java/EndPointExerciciosTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class EndPointExerciciosTests {
     // 1º Teste: verifica se a variável passada por Query Param é a mesma retornada no payload do response:
     @Test
-    public void exercicio01_TesteParOuImpar() {
+    public void exercicio04_TesteParOuImpar() {
         // Armazena a URL base e o endpoint que será testado:
         String url = "http://localhost:8080/exercicios/parOuImpar";
         // Armazena o Valor da chave do query param:
@@ -19,22 +19,52 @@ public class EndPointExerciciosTests {
         RestAssured.given()
                 // Passando o parâmetro e valor do query param:
                 .queryParam(chaveQueryParam, valorQueryParam)
-                // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request):
-                .log().all()
-                // Quando: Executa a ação principal do teste (disparo do verbo HTTP):
+                    // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request):
+                    .log().all()
+                    // Quando: Executa a ação principal do teste (disparo do verbo HTTP):
                 .when()
-                // Envia uma requisição HTTP do tipo GET para o endereço da variável 'url':
-                .get(url)
+                    // Envia uma requisição HTTP do tipo GET para o endereço da variável 'url':
+                    .get(url)
                 // Então: Inicia o bloco de validações e asserções da resposta (Response):
                 .then()
-                // Imprime no console todos os detalhes do que foi RECEBIDO do servidor:
-                .log().all()
-                // Metodo de transição BDD que melhora a leitura do código (afirme que...).
-                // Indica que a partir daqui virão às asserções:
-                .assertThat()
-                // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK):
-                .statusCode(200)
-                // Valida se o corpo (body) da resposta contém a frase/texto especificado:
-                .body(Matchers.containsString(mensagemResponseQueryParam));
+                    // Imprime no console todos os detalhes do que foi RECEBIDO do servidor:
+                    .log().all()
+                    // Metodo de transição BDD que melhora a leitura do código (afirme que...).
+                    // Indica que a partir daqui virão às asserções:
+                    .assertThat()
+                    // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK):
+                    .statusCode(200)
+                    // Valida se o corpo (body) da resposta contém a frase/texto especificado:
+                    .body(Matchers.containsString(mensagemResponseQueryParam));
+    }
+
+    @Test
+    public void exercicio05_TesteParOuImpar() {
+        // Armazena o Valor do path param:
+        String salarioPathParam = "2600";
+        // Armazena a URL base e o endpoint que será testado:
+        String url = "http://localhost:8080/exercicios/calculaSalario/" + salarioPathParam;
+        // Mensagem do response a ser validada:
+        String mensagemResponsePathParam = "Salario bruto = 2600.0";
+
+        // Dado que: Inicia a preparação da requisição com a chamada explícita da classe RestAssured:
+        RestAssured.given()
+                    // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request):
+                    .log().all()
+                // Quando: Executa a ação principal do teste (disparo do verbo HTTP):
+                .when()
+                    // Envia uma requisição HTTP do tipo GET para o endereço da variável 'url':
+                    .get(url)
+                // Então: Inicia o bloco de validações e asserções da resposta (Response):
+                .then()
+                    // Imprime no console todos os detalhes do que foi RECEBIDO do servidor:
+                    .log().all()
+                    // Metodo de transição BDD que melhora a leitura do código (afirme que...).
+                    // Indica que a partir daqui virão às asserções:
+                    .assertThat()
+                    // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK):
+                    .statusCode(200)
+                    // Valida se o corpo (body) da resposta contém a frase/texto especificado:
+                    .body(Matchers.containsString(mensagemResponsePathParam));
     }
 }
