bb9697c76a12da73f26800189d615078c82fc92f
Tiago Mota <tmota.seg@gmail.com>
2026-09-12 19:28:51 -0300

    Feat: Deixando os comentários mais claros.

--- restassured-testeapi/src/test/java/apifutebol/ApiFutebolTests.java
+++ restassured-testeapi/src/test/java/apifutebol/ApiFutebolTests.java
@@ -8,19 +8,20 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
 public class ApiFutebolTests {
-    // 3º Teste: verifica se a variável passada por Query Param é a mesma retornada no payload do response:
+    // 1º Teste: Valida a autenticação por Token Bearer e verifica a presença do campeonato no payload de resposta:
     @Test
     public void exercicio07_TesteApiFutebolAutenticacao() {
         // Armazena a URL base e o endpoint que será testado:
         String url = "https://api.api-futebol.com.br/v1/campeonatos";
 
-        // Armazena a mensagem do response do query param:
+        // Armazena o texto esperado para validação no corpo da resposta:
         String mensagemResponse = "Campeonato Baiano 2026";
 
-        // Dado que: Inicia a preparação da requisição com a chamada explícita da classe RestAssured:
+        // Dado que: Inicia a preparação da requisição (usando o import estático 'given()'):
         given()
             // Imprime no console todos os detalhes do que está SENDO ENVIADO (Request):
             .log().all()
+            // Adiciona o cabeçalho de autorização (Header Authorization) com o Token Bearer:
             .header("Authorization", "Bearer live_fede9295a60b13b55f2714314f5d7b")
         // Quando: Executa a ação principal do teste (disparo do verbo HTTP):
         .when()
@@ -30,12 +31,11 @@ public class ApiFutebolTests {
         .then()
             // Imprime no console todos os detalhes do que foi RECEBIDO do servidor:
             .log().all()
-            // Metodo de transição BDD que melhora a leitura do código (afirme que...).
-            // Indica que a partir daqui virão às asserções:
+            // Metodo de transição BDD que indica o início das asserções (afirme que...):
             .assertThat()
             // Valida se o código de status HTTP retornado pelo servidor é igual a 200 (OK):
             .statusCode(200)
-            // Valida se o corpo (body) da resposta contém a frase/texto especificado:
+            // Valida se o corpo (body) da resposta contém a frase/texto especificado (usando o import estático 'containsString'):
             .body(containsString(mensagemResponse));
     }
 }
