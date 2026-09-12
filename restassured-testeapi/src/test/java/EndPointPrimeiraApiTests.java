3cdca240e0b8311d4cd2645d3d618f68d3d7c3f9
Tiago Mota <tmota.seg@gmail.com>
2026-09-12 12:38:05 -0300

    Feat: Início da implementação da RequestSpecificatio - com a declaração vavariável de mesmo tipo -, que é uma interface do Rest Assured que funciona como um molde ou contrato pré-configurado para as requisições, que deixa o código mais enxuto e legível.

--- restassured-testeapi/src/test/java/EndPointPrimeiraApiTests.java
+++ restassured-testeapi/src/test/java/EndPointPrimeiraApiTests.java
@@ -3,11 +3,14 @@
 import io.restassured.RestAssured;
 // Importa a classe Matchers do framework Hamcrest,
 // responsável por fornecer métodos comparadores (como containsString, equalTo, hasSize) para validações nos testes
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 // Import do JUnit 4:
 import org.junit.Test;
 
 public class EndPointPrimeiraApiTests {
+    // Variável que receberá as regras da requisição definidas na classe 'RequestSpecBuilder':
+    RequestSpecification requestSpec;
 
     // 1º Teste: Valida se a mensagem de sucesso foi retornada no payload do response:
     // O '@Test' Indica ao JUnit que este metodo é um teste automatizado a ser executado
